eb606fd03d34c50d506fb422f06480d72743eb6e
Jon Schulman <jon@js-tbpro13.local>
2020-01-29 13:56:18 -0800

    add back required tag plus another one

--- main.tf
+++ main.tf
@@ -150,8 +150,10 @@ resource "aws_instance" "web" {
   }
   tags = {
     AppName    = "TFDemoApp"
+    AppOwner   = "Jon"
     CostCenter = "TFE-PM-0001"
     Name       = "Clarity TF Demo App"
+    DemoTag    = "HashiCorp Webinar"
   }
 }
 
